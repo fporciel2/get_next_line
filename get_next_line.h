b0de7253375ce0ff81b5140d24ca0fe33e689ede
Federico Porciello <fporciel@e4r7p4.42roma.it>
2023-03-11 10:00:13 +0100

    Started the project.

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 17:28:34 by fporciel          #+#    #+#             */
-/*   Updated: 2023/03/09 17:29:29 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/03/11 09:59:24 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8192
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
